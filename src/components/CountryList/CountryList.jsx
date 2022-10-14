1bbd4a56e8cdf10c626f7d539c2ddee239a85aea
Tripoli80 <tripoli80@gmail.com>
2022-10-14 22:07:51 +0200

    +

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,5 +1,21 @@
 import { Grid, GridItem } from 'components';
-
+import { Link, useLocation } from 'react-router-dom';
 export const CountryList = ({ countries }) => {
-  return <h2>CountryList</h2>;
+  const location = useLocation();
+  if (!countries.length) return;
+
+  return (
+    <Grid>
+      {countries.map(({ id, flag, country }) => {
+        return (
+          <GridItem key={id}>
+            <Link to={`/country/${id}`} state={{ from: location }}>
+              <img src={flag} alt={country} title={country} />
+            </Link>
+            {/* <p>{country}</p> */}
+          </GridItem>
+        );
+      })}
+    </Grid>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/country-service';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setCountries([]);
+      setIsLoading(true);
+      const data = await getCountries();
+      console.log('🚀 ~ data', data);
+      setCountries(data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        {isLoading && <Loader />}
+        {countries.length>0 &&<CountryList countries={countries} />}
       </Container>
     </Section>
   );
